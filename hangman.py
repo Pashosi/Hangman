9aa66df7c817fe5651f2a96f22e0d06ba1daac72
Pavel Bramanis <bramanis2@gmail.com>
2024-02-25 11:02:42 +0300

    Added letter filter and image hangman

--- hangman.py
+++ hangman.py
@@ -1,5 +1,6 @@
 import sys
 from random import choice
+import re
 
 HANGMAN = (
     """
@@ -83,33 +84,39 @@ def get_words():
 def greeting():  # приветствие
     print('Начинаем игру?: y-Да, n-Нет ')
     n = input()
-    if n in ['y', 'да', 'го', 'yes']:
+    if n in ['y', 'да', 'го', 'yes', 'д']:
         word = get_words()
-        print('*' * len(word))
         input_letter(word)
+
     else:
         print('Ну ладно')
 
 
 def input_letter(word: str):
-    # print(word)
     count = 0
+    print(HANGMAN[count])
+    print('*' * len(word))
     for i in sys.stdin:
-        if i.strip('\n') in word:
-            word = ''.join([letter.upper() if (i.strip('\n') == letter) else letter for letter in word])
-            print(''.join(['*' if i.islower() else i for i in word]))
-            if all(i.isupper() for i in word):
-                print('Вы победили')
-                greeting()
-        elif i.strip('\n').upper() in word:
-            print('Такую букву уже вводили')
+        if bool(re.search(r'[а-яА-Я]', i.strip())):
+            if i.strip('\n') in word:
+                word = ''.join([letter.upper() if (i.strip('\n') == letter) else letter for letter in word])
+                print(''.join(['*' if i.islower() else i for i in word]))
+                if all(i.isupper() for i in word):
+                    print('Вы победили')
+                    greeting()
+            elif i.strip('\n').upper() in word:
+                print('Такую букву уже вводили')
+            else:
+                try:
+                    count += 1
+                    print(HANGMAN[count])
+                    print(''.join(['*' if i.islower() else i for i in word]))
+                except IndexError:
+                    print('Вы проиграли')
+                    greeting()
         else:
-            try:
-                print(HANGMAN[count])
-                count += 1
-            except IndexError:
-                print('Вы проиграли')
-                greeting()
+            print('Не правильный символ')
+
 
 
 greeting()
